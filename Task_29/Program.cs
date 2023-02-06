b6a437ae6f3e4d1b1be03c626fc29d332f141f9d
Artyom <artyombagaev150@yandex.ru>
2023-02-06 19:53:15 +0300

    task_29

---
+++ Task_29/Program.cs
@@ -0,0 +1,28 @@
+﻿/* Задача 29: Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.
+1, 2, 5, 7, 19 -> [1, 2, 5, 7, 19]
+6, 1, 33 -> [6, 1, 33]*/
+
+
+
+
+int[] ReadArray()
+{
+    int[] mass = new int[8];
+    for (int i = 0; i < 8; i++)
+    {
+        mass[i]=int.Parse(Console.ReadLine()!);
+    }
+    return mass;
+}
+
+void PrintArray (new[] array)
+{
+    int count = Array.Lenght;
+    for (int i = 0; i<count; i++)
+    {
+        Console.Write($"{Array[i]}, ");
+    }
+}
+
+int[] array = ReadArray();
+PrintArray(Array);
